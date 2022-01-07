1d117ea477168b1092853f883dfe3e2a3f808687
napolanocarmine <58261398+napolanocarmine@users.noreply.github.com>
2022-01-07 16:07:57 +0100

    add file test label_map and changed r and c

---
+++ test_label_map.py
@@ -0,0 +1,8 @@
+from combinatorial.notebooks.combinatorial.pixelmap import LabelMap, PixelMap
+from pixel_map_z_curve_full import D
+from combinatorial.notebooks.combinatorial.zoo import G2_TWO_TRIANGLES_1
+
+#pm = PixelMap(G2_TWO_TRIANGLES_1)
+lm = LabelMap.from_shape(2,2)
+
+lm.plot_faces()
